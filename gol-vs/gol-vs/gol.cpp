7c701509c59f5c59d204330ecc6d302c007d6ac0
don <97c8caf0-dc2e-4bdc-aaeb-91ce444be156@anonaddy.me>
2021-02-16 09:32:31 +0900

    thread skeleton

--- gol-vs/gol-vs/gol.cpp
+++ gol-vs/gol-vs/gol.cpp
@@ -1,11 +1,44 @@
+#include <pthread.h>
+#include <iostream>
+
 #include "gol.h"
 
-void run(
-  std::vector< std::tuple<int,int> > initial_population,
+// return vector of coordinates of the alive cells of the final population
+std::vector<std::tuple<int,int>> run(
+  std::vector<std::tuple<int,int>> initial_population,
   int num_iter,
   int max_x,
   int max_y
 ) {
+  int const numThr = max_x * max_y;
+  pthread_t* threads = new pthread_t [numThr];
+  pthread_attr_t attr;
 
+  // init and set thr joinable
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  for (int i = 0; i < numThr; ++i) {
+    //int rc = pthread_create(&threads[i], &attr, , (void*));
+    //if (rc) {
+    //  std::cout << "Err: unable to create thread, " << rc << std::endl;
+    //  exit(-1);
+    //}
+  }
+
+  pthread_attr_destroy(&attr);
+  for (int i = 0; i < numThr; ++i)
+  {
+    void* status;
+    int rc = pthread_join(threads[i], &status);
+    if (rc) {
+      std::cout << "Err: unable to join, " << rc << std::endl;
+      exit(-1);
+    }
+  }
+
+  pthread_exit(NULL);
+
+  return {};
 }
 
